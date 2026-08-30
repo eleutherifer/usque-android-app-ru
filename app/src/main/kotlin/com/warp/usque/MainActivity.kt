726769016a44032a42e4735da399bac33fad7dd5
eleutherifer <eleutherifer@gmail.com>
2026-08-30 23:09:33 +0300

    Исправление  профилей по умолчанию.

--- app/src/main/kotlin/com/warp/usque/MainActivity.kt
+++ app/src/main/kotlin/com/warp/usque/MainActivity.kt
@@ -778,16 +778,18 @@ class MainActivity : Activity() {
             }
         }
         if (profiles.isEmpty()) {
-            profiles["speed.cloudflare.com:443:162.159.198.2 (loc 1) HTTP/3"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "auto")
-            profiles["speed.cloudflare.com:443:162.159.198.2 (loc 1) HTTP/3"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "http3")
-            profiles["speed.cloudflare.com:443:162.159.199.2 (loc 2) HTTP/3"] = Profile("speed.cloudflare.com", "162.159.199.2", 443, "http3")
-            profiles["speed.cloudflare.com:443:162.159.198.2 (loc 1) HTTP/2"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "http2")
-            profiles["speed.cloudflare.com:443:162.159.199.2 (loc 2) HTTP/2"] = Profile("speed.cloudflare.com", "162.159.199.2", 443, "http2")
-            profiles["deepseek.com:443:162.159.198.2 (loc 1) HTTP/3"] = Profile("deepseek.com", "162.159.198.2", 443, "auto")
-            profiles["deepseek.com:443:162.159.198.2 (loc 1) HTTP/3"] = Profile("deepseek.com", "162.159.198.2", 443, "http3")
-            profiles["deepseek.com:443:162.159.199.2 (loc 2) HTTP/3"] = Profile("deepseek.com", "162.159.199.2", 443, "http3")
-            profiles["deepseek.com:443:162.159.198.2 (loc 1) HTTP/2"] = Profile("deepseek.com", "162.159.198.2", 443, "http2")
-            profiles["deepseek.com:443:162.159.199.2 (loc 2) HTTP/2"] = Profile("deepseek.com", "162.159.199.2", 443, "http2")
+            profiles["speed.cloudflare.com, loc 1, Auto"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "auto")
+            profiles["speed.cloudflare.com, loc 1, HTTP/3"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "http3")
+            profiles["speed.cloudflare.com, loc 1, HTTP/2"] = Profile("speed.cloudflare.com", "162.159.198.2", 443, "http2")
+            profiles["speed.cloudflare.com, loc 2, Auto"] = Profile("speed.cloudflare.com", "162.159.199.2", 443, "auto")
+            profiles["speed.cloudflare.com, loc 2, HTTP/3"] = Profile("speed.cloudflare.com", "162.159.199.2", 443, "http3")
+            profiles["speed.cloudflare.com, loc 2, HTTP/2"] = Profile("speed.cloudflare.com", "162.159.199.2", 443, "http2")
+            profiles["deepseek.com, loc 1, Авто"] = Profile("deepseek.com", "162.159.198.2", 443, "auto")
+            profiles["deepseek.com, loc 1, HTTP/3"] = Profile("deepseek.com", "162.159.198.2", 443, "http3")
+            profiles["deepseek.com, loc 1, HTTP/2"] = Profile("deepseek.com", "162.159.198.2", 443, "http2")
+            profiles["deepseek.com, loc 2, Auto"] = Profile("deepseek.com", "162.159.199.2", 443, "auto")
+            profiles["deepseek.com, loc 2, HTTP/3"] = Profile("deepseek.com", "162.159.199.2", 443, "http3")
+            profiles["deepseek.com, loc 2, HTTP/2"] = Profile("deepseek.com", "162.159.199.2", 443, "http2")
             persistProfiles()
         }
         refreshProfileSpinner()
